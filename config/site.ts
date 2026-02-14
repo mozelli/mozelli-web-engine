e7d3ce5b7d6a134444f5414fd8bf2517856a5dd7
João Mozelli Neto <joaomozelli@hotmail.com>
2026-02-14 11:45:11 -0300

    alteração base url

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Mozelli Web Engine",
   description: "Boilerplate profissional para produção rápida de websites",
-  url: "http://localhost:3333",
+  url: "http://localhost:3000",
   ogImage: "/og-image.png",
   links: {
     instagram: "",
